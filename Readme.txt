4dc3d73aeed349187b429bec15343fa59548ff48
Rikhardson <n2puri00@students.oamk.fi>
2024-03-13 00:00:23 +0200

    yup

--- Readme.txt
+++
@@ -1,3 +0,0 @@
-npm install react-native-paper
-npm install react-native-safe-area-context
-npm i react-native-calendars
---
+++ styles/Theme.js
@@ -0,0 +1,17 @@
+import { MD3LightTheme as MyTheme } from 'react-native-paper';
+
+export const theme = {
+    ...MyTheme,
+    colors: {
+      ...MyTheme.colors,
+      primary: "#000000", 
+      onSecondaryContainer: "#b9d4f8", 
+      secondaryContainer: "#5f9bee",
+      background: "#ffffff",
+      surface: "#bebedd",
+      onSurface: "#1767d6", 
+      surfaceVariant: "#cecbd0",
+      onSurfaceVariant: "#5f9bee", 
+      outline: "#0d3c7d", 
+    },
+  };
